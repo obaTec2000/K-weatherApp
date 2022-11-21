ba180e3f1f4d47f4dfeba4afd7bd0587cca20e9f
Obanijesu <obanijesujohn45@gmail.com>
2022-11-21 10:33:20 +0100

    weather app

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "af24da9a3dmsh7b328cc896670e7p17d1b3jsn4bc2e7406be2",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "21d7b824e8b8b810f8ffe56532a3226b";
